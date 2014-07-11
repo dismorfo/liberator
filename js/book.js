0abfc31798623e05b458eefdd068b794f2c2317a
Alberto Ortiz Flores <dismorfo@gmail.com>
2014-07-11 19:22:57 -0400

    update js

--- js/book.js
+++ js/book.js
@@ -6,37 +6,46 @@ YUI().use(
   , function (Y) {
   
     'use strict'
-    
-    // http://stackoverflow.com/questions/3811913/firing-events-between-an-iframe-and-its-parent-using-yui  
 
     var body = Y.one('body')
       , topOffsetHeight = Y.one('.home-menu').get('offsetHeight')
       , iframe = Y.one('iframe')
       , match = location.pathname.match(/\/book\/(.*)\/(.*)/)
+      , match_page = location.pathname.match(/\/book\/(.*)\/(.*)\/(.*)/)
       , viewport = Y.DOM.viewportRegion()
       , src
 
     iframe.setStyles({
       top: topOffsetHeight,
-      visibility: 'hidden', // wait until the book is available to show it
-      opacity: '0', // wait until the book is available to show it
       height: viewport.height - topOffsetHeight
     })
 
     if (
-        match 
-        &&
-        match[2]  
-    ) {
-      src = body.getAttribute("data-sourceUrl") + '/books/' + match[2] + '/1';
+         match_page
+         &&
+         match_page[3]
+      ) 
+    {
+      src = body.getAttribute("data-sourceUrl") + '/books/' + match_page[2] + '/' +  match_page[3];
     }
-
+    
+    else if (
+         match 
+         &&
+         match[2]
+      ) 
+    {
+      src = body.getAttribute("data-sourceUrl") + '/books/' + match[2] + '/1';
+    }    
+    
     iframe.set('src', src)
     
-    Y.Global.on("crossframe:message", function (e, data, callback) { });
-
     // https://github.com/josephj/yui3-crossframe
     iframe.on('load', function() {  
+    
+        function onClick(e) {
+            Y.log('CLICK')
+        }
 
         var frameName = 'frames["book"]'
           , message   = body.getAttribute("data-app") + '/css/book.css'
@@ -60,6 +69,14 @@ YUI().use(
           };
 
         Y.CrossFrame.postMessage(frameName, message, config)
+
+        Y.Global.on("crossframe:message", function (e, data, callback) {
+        
+            Y.log(
+              data
+            )
+            
+        });
     
     })
 
